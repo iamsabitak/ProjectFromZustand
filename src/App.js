437ec272aeee5760afb042b2387d030fb2f42425
sabita <sabitakhadka091@gmail.com>
2024-03-05 11:54:38 +0545

    added the counter component to the app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BearCounter from "./components/BearCounter";
 import { Controls } from "./components/Controls";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
       <BearCounter />
       <Controls />
+      <Counter />
     </div>
   );
 }
