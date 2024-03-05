938013e37a3e7a59d00b60013f2bfdd7e9df8769
sabita <sabitakhadka091@gmail.com>
2024-03-05 14:39:50 +0545

    Implemented functionality to add todo items with unique IDs

--- src/todo/AddTodo.jsx
+++ src/todo/AddTodo.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { store } from "../zustand/store";
 
 function AddTodo() {
   const [input, setInput] = useState();
+
+  const addtodos = () => store.getState().addTodo({ input, id: Date.now() });
+
   return (
     <div>
       <input
@@ -9,6 +13,7 @@ function AddTodo() {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
+      <button onClick={addtodos}>add Todo</button>
     </div>
   );
 }
