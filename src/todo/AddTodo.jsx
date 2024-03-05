b9174a580d6442732e6fea78d6662757802d9bd6
sabita <sabitakhadka091@gmail.com>
2024-03-05 15:06:20 +0545

    Added functionality to add and remove todo items

--- src/todo/AddTodo.jsx
+++ src/todo/AddTodo.jsx
@@ -2,18 +2,34 @@ import React, { useState } from "react";
 import { store } from "../zustand/store";
 
 function AddTodo() {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
 
-  const addtodos = () => store.getState().addTodo({ input, id: Date.now() });
+  const addtodos = () =>
+    store.getState().addTodo({ text: input, id: Date.now() });
+
+  const todos = store((state) => state.todos);
+
+  const removetodos = (id) => store.getState().removeTodo(id);
 
   return (
     <div>
       <input
         type="text"
+        name="todos"
+        id="todos"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
       <button onClick={addtodos}>add Todo</button>
+
+      <div>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => removetodos(todo.id)}>remove</button>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
