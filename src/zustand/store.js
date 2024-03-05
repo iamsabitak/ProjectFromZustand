a358496cbb29f724b5e6cf338517b3dc17b464bf
sabita <sabitakhadka091@gmail.com>
2024-03-05 15:06:42 +0545

    fix: Ensure todos are correctly added and displayed in AddTodo component

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -11,7 +11,8 @@ export const store = create((set) => ({
   increase: () => set((state) => ({ count: state.count + 1 })),
   decrease: () => set((state) => ({ count: state.count - 1 })),
 
-  addTodo: (newTodo) => set((state) => ({ ...state, newTodo })),
+  addTodo: (newTodo) =>
+    set((state) => ({ ...state, todos: [...state.todos, newTodo] })),
   removeTodo: (id) =>
     set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
 }));
