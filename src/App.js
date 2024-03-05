be75c71e90d5ccdac120112afe1a67eacfa9b888
sabita <sabitakhadka091@gmail.com>
2024-03-05 14:40:14 +0545

    added addTodo ti the app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import BearCounter from "./components/BearCounter";
-import { Controls } from "./components/Controls";
-import Counter from "./components/Counter";
+import AddTodo from "./todo/AddTodo";
+// import BearCounter from "./components/BearCounter";
+// import { Controls } from "./components/Controls";
+// import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      <BearCounter />
+      {/* <BearCounter />
       <Controls />
-      <Counter />
+      <Counter /> */}
+      <AddTodo />
     </div>
   );
 }
