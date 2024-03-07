f60ddb0aa0afe751bc041166e6cebf031d108fc7
sabita <sabitakhadka091@gmail.com>
2024-03-07 10:34:48 +0545

    saved the bearcounter in localstorage

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -1,18 +1,29 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-export const store = create((set) => ({
-  bears: 0,
-  count: 0,
-  todos: [],
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
+export const store = create(
+  persist(
+    (set, get) => ({
+      bears: 0,
+      count: 0,
+      todos: [],
+      increasePopulation: () => set({ bears: get().bears + 1 }),
+      removeAllBears: () => set({ bears: 0 }),
+      updateBears: (newBears) => set({ bears: newBears }),
 
-  increase: () => set((state) => ({ count: state.count + 1 })),
-  decrease: () => set((state) => ({ count: state.count - 1 })),
+      increase: () => set((state) => ({ count: state.count + 1 })),
+      decrease: () => set((state) => ({ count: state.count - 1 })),
 
-  addTodo: (text) =>
-    set((state) => ({ todos: [...state.todos, { text, id: Date.now() }] })),
-  removeTodo: (id) =>
-    set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
-}));
+      addTodo: (text) =>
+        set((state) => ({ todos: [...state.todos, { text, id: Date.now() }] })),
+      removeTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
+    }),
+    {
+      name: "bear-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
