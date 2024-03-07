6172e3f1ac6bb0ff69167c61eb4332133836ab04
sabita <sabitakhadka091@gmail.com>
2024-03-07 12:10:51 +0545

    added the functionlaty of add and remove name

--- src/user/Username.jsx
+++ src/user/Username.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { store } from "../zustand/store";
 
 function Username() {
+  const [name, setName] = useState("");
+  const { addName, removeName, user } = store();
+  const onHandleSubmit = () => {
+    addName(name);
+  };
+  const onHandleRemove = (id) => {
+    removeName(id);
+  };
   return (
-    <div>Username</div>
-  )
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onHandleSubmit}>add</button>
+
+      <>
+        {user.map((name) => (
+          <>
+            {" "}
+            <li key={name.id}>{name.name}</li>
+            <button onClick={() => onHandleRemove(name.id)}>delete</button>
+          </>
+        ))}
+      </>
+    </div>
+  );
 }
 
-export default Username
+export default Username;
