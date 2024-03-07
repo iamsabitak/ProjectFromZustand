99fb67654588fa07ca8104a508f2db1de5a7b719
sabita <sabitakhadka091@gmail.com>
2024-03-07 13:34:43 +0545

    fixed some small problems

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 export const store = create(
   persist(
@@ -34,7 +34,6 @@ export const store = create(
     }),
     {
       name: "storage",
-      storage: createJSONStorage(() => localStorage),
     }
   )
 );
