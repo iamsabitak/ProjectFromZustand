e8f66d89a5d296cd0e40b9b10156122e2cbe9961
sabita <sabitakhadka091@gmail.com>
2024-03-07 13:36:37 +0545

    implemented the counter and addtodo app to the PP.JS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 // import BearCounter from "./bearcounter/BearCounter";
 // import { Controls } from "./bearcounter/Controls";
-// import AddTodo from "./todo/AddTodo";
+import AddTodo from "./todo/AddTodo";
 
-// import Counter from "./counter/Counter";
-import Username from "./user/Username";
+import Counter from "./counter/Counter";
+// import Username from "./user/Username";
 
 function App() {
   return (
     <div>
-      {/* <BearCounter />
-      <Controls />
+      {/* <BearCounter /> */}
+      {/* <Controls /> */}
       <Counter />
-      <AddTodo /> */}
-      <Username />
+      <AddTodo />
+      {/* <Username /> */}
     </div>
   );
 }
